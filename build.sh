46d3bf85e0bede818642f4287707c1b2707dad4a
Haider Zahoor <haider2090@gmail.com>
2017-12-12 19:06:49 +0500

    Update build.sh
    Stupid mistake covered up

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
  export SUBARCH=arm64
  
- #Defconfig for Moto C
--make -C $PWD O=$PWD/out ARCH=arm64 wt6737m_65_n_defconfig
-+make -C $PWD O=$PWD/out ARCH=arm 64 rlk6737m_open_n_defconfig
+ #Defconfig for X556 (originally Moto C)
+
+ make -C $PWD O=$PWD/out ARCH=arm 64 rlk6737m_open_n_defconfig
  #make ARCH=arm64 wt6737m65_n_defconfig
  
  make -j4 -C $PWD O=$PWD/out ARCH=arm64
